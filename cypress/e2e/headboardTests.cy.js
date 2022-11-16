662e9b683b54c682b1f15d1d40d85b28658e4357
YagoHD <yagorb@hotmail.com>
2022-11-16 09:30:55 +0100

    ADD: Proyect creation,first trial test

---
+++ cypress/e2e/headboardTests.cy.js
@@ -0,0 +1,7 @@
+/// <reference types="cypress"/>
+describe('Tests Headboard', () => {
+    it('First we connect to the web to check that it works', () => {
+        cy.visit('https://www.demoblaze.com/')
+    })
+
+})
