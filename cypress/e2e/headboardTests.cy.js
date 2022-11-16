dc702fe9cc83c3a6cc2934eaeec49a5b4c29ed4c
YagoHD <yagorb@hotmail.com>
2022-11-16 11:05:19 +0100

    ADD: Tests sin completar y test que da error. En este punto tengo que darle unas vueltas a la documentación por que estoy atascado

--- cypress/e2e/headboardTests.cy.js
+++ cypress/e2e/headboardTests.cy.js
@@ -4,4 +4,29 @@ describe('Tests Headboard', () => {
         cy.visit('https://www.demoblaze.com/')
     })
 
+
+    it('Click on product store or web logo', () => {
+        //cy.get('#nava').click().should(performance.navigation.type).to.eq(performance.navigation.TYPE_RELOAD)
+
+        //     cy.get('#nava').click().then((response) => {
+    //         expect(response.status).to.eq(200)
+    //     })
+        cy.get('#nava').click()
+    })
+
+    it('Click on Home', () => {
+        cy.get('.active > .nav-link').click()
+    })
+
+    it('Click on Contact', () => {
+    })
+
+    it('Click on Contact-Close',() => {
+        cy.on('#exampleModal')
+        cy.get('#exampleModal > .modal-dialog > .modal-content > .modal-footer > .btn-secondary').click()
+        })
+    
+
+ 
+
 })
