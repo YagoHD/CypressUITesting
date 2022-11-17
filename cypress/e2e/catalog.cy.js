4cbb1c25de054661206cf3b80f04c2d6548a814d
YagoHD <yagorb@hotmail.com>
2022-11-17 10:55:33 +0100

    ADD: Tests Catalog realizados

--- cypress/e2e/catalog.cy.js
+++ cypress/e2e/catalog.cy.js
@@ -1,27 +1,36 @@
 /// <reference types="cypress"/>
 describe('Tests Catalog', () => {
-    beforeEach(()=>{
-        cy.visit('https://www.demoblaze.com/')
-    })
-
     it('Click on product Name', () => {
+        cy.visit('https://www.demoblaze.com/')
+
         cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').click()
+        cy.get('.name').should('have.text', 'Samsung galaxy s6')
     })
 
     it('Click on product Image', () => {
+        cy.visit('https://www.demoblaze.com/')
+
         cy.get(':nth-child(1) > .card > :nth-child(1) > .card-img-top').click()
+        cy.get('.name').should('have.text', 'Samsung galaxy s6')
     })
 
     it('Click on product Description', () => { 
+        cy.visit('https://www.demoblaze.com/')
+
         cy.get(':nth-child(1) > .card > .card-block > #article').click()
     })
 
     it('Click on next', () => {
-        cy.get('#prev2').click()
+        cy.visit('https://www.demoblaze.com/')
+        cy.get('#next2').click(cy.wait(5000))
+        cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').should('have.text', 'Apple monitor 24')
+
     })
 
     it('Click on previous', () => {
-        cy.get('#next2').click()
+        cy.visit('https://www.demoblaze.com/')
+        cy.get('#prev2').click(cy.wait(5000))
+        cy.get(':nth-child(1) > .card > .card-block > .card-title > .hrefch').should('have.text', 'Nokia lumia 1520')
     })
 
 })
